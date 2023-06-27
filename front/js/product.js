0bd1a93b019c59aa3a0265ff99c7db717603a806
saxopa <alexispontikis1@gmail.com>
2023-06-27 03:30:03 +0200

    Création du panier & ajout localStorage

--- front/js/product.js
+++ front/js/product.js
@@ -2,63 +2,102 @@
 const params = new URL(document.location).searchParams;
 // Je stocke l'id dans une variable
 const id = params.get("id");
-//je vérifie si ça fonctionne bien 
-console.log("hello l'ID de cette article est : " + id);
+// Je vérifie si ça fonctionne bien
+console.log("L'ID de cet article est : " + id);
 // Je stocke l'URL de l'API dans une variable en y ajoutant l'id
 const localUrl = `http://localhost:3000/api/products/${id}`;
 
-
 const colorsSelect = document.getElementById("colors");
+const quantity = document.getElementById("quantity");
 
 // Je sélectionne les éléments du DOM
 const titleElement = document.getElementById("title");
 const priceElement = document.getElementById("price");
 const descriptionElement = document.getElementById("description");
 const containerImg = document.querySelector(".item__img");
+const buttonCart = document.getElementById("addToCart");
 
 // Fonction pour récupérer et afficher les détails de l'article
 const fetchArticle = () => {
   fetch(localUrl)
     .then((res) => res.json())
     .then((data) => {
-      console.log(data);
+      //console.log(data);
 
-        // J'ajoute l'image à l'élément img
-        const photo = document.createElement("img");
-        photo.src = data.imageUrl;
-        containerImg.appendChild(photo);
+      // J'ajoute l'image à l'élément img
+      const photo = document.createElement("img");
+      photo.src = data.imageUrl;
+      containerImg.appendChild(photo);
 
       // J'affiche les données dans les éléments correspondants
       titleElement.textContent = data.name;
       priceElement.textContent = data.price;
       descriptionElement.textContent = data.description;
 
-     //j'affiche les couleurs dans le select
-     data.colors.forEach((color) => {
+      // J'affiche les couleurs dans le select
+      data.colors.forEach((color) => {
         let option = document.createElement("option");
         option.value = color;
         option.textContent = color;
         colorsSelect.appendChild(option);
       });
 
-      //limites de la quantité à 100 articles
-        let quantity = document.getElementById("quantity");
-        for (let i = 1; i <= 100; i++) {
-            let option = document.createElement("option");
-            option.value = i;
-            option.textContent = i;
-            quantity.appendChild(option);
-            }
-    
-    
+      // Limites de la quantité à 100 articles
+      for (let i = 1; i <= 100; i++) {
+        let option = document.createElement("option");
+        option.value = i;
+        option.textContent = i;
+        quantity.appendChild(option);
+      }
     })
+    // Je récupère les erreurs du fetch et les affiche dans la console
     .catch((error) => {
       console.log(error);
     });
 };
-  
+
+// Vérification ajout au panier
+buttonCart.addEventListener("click", (e) => {
+  if (validationCart() === "") {
+    console.log("OK");
+  } else {
+    // Affichage d'un message d'erreur
+    console.log(validationCart());
+  }
+});
+
+function validationCart() {
+  let message = "";
+  // Validation quantité
+  if (quantity.value < 1 || quantity.value > 100) {
+    message = "La quantité doit être comprise entre 1 et 100";
+  }
+  return message;
+}
+
+// Ajout du produit au panier
+buttonCart.addEventListener("click", () => {
+  // Création d'un objet pour stocker les données du produit
+  const addProduct = {
+    id: id,
+    quantity: quantity.value,
+    color: colorsSelect.value,
+  };
+  // Test de l'ajout du produit sur la console
+  //console.log(addProduct); 
+
+                                            /* Stockage des données dans le local storage */
+
+  // Création d'un tableau pour stocker les produits
+  let products = [];
+  if (localStorage.getItem("buttonCart") !== null) {
+    products = JSON.parse(localStorage.getItem("buttonCart"));
+    products.push(addProduct);
+    localStorage.setItem("buttonCart", JSON.stringify(products));
+  } else {
+    console.log("Le panier est vide");
+  }
+});
 
 // Appel de la fonction pour récupérer et afficher les détails de l'article
 fetchArticle();
-
-
